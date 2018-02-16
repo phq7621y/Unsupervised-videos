a7518ac20aff0e0d9d3edb681be3a53f5e451532
shreeyamody <modyshreeya@gmail.com>
2018-02-16 11:31:53 -0700

    loading data

--- Main.py
+++ Main.py
@@ -36,15 +36,17 @@ fc0 = tf.contrib.layers.fully_connected(c2, 1, activation_fn=None)
 def load_data():
     global train_input_seq, train_output_seq, test_input_seq, test_output_seq
 
-    data = np.load( '../data/mnist_test_seq.npy' )
+    data = np.load( 'datasets/mnist_test_seq.npy' )
     # ['clips', 'dims', 'input_raw_data']
     #(200K, 1, 64, 64) --> (10K, 20, 64, 64)-> number of sequences, frames/sequence, height, width
     data = np.reshape( data, [-1, 20, 64, 64, 1] )
     print("loading training data: data.shape", data.shape)
     train_input_seq = data[0:8000, 0:10]
-    train_output_seq = data[0:8000, 10:]
+    train_output_seq = data[0:8000, 10:0]
+    train_future_seq = data[0:8000, 10:]
     test_input_seq = data[8000:, 0:10]
-    test_output_seq = data[8000:, 10:]
+    test_output_seq = data[8000:, 10:0]
+    train_future_seq = data[0:8000,10:]
 
     return
 
@@ -91,7 +93,7 @@ load_data()
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
-    summary_writer = tf.summary.FileWriter(_,graph=sess.graph)
+    # summary_writer = tf.summary.FileWriter(_,graph=sess.graph)
 
     for e in range(epochs):
 
