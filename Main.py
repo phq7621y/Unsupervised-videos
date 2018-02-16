6b3165354f9aa2f126c3b91140b32d090a5d676e
shreeyamody <modyshreeya@gmail.com>
2018-02-16 11:19:04 -0700

    loading data

--- Main.py
+++ Main.py
@@ -5,6 +5,13 @@ import math
 import matplotlib.pyplot as plt
 from skimage import io, transform
 
+#global variables
+train_input_seq = [None]
+train_output_seq = [None]
+test_input_seq = [None]
+test_output_seq = [None]
+
+
 #parameters
 batch = 1
 epochs = 1000
@@ -12,8 +19,8 @@ frames = 10
 
 #placeholders
 #enc_in = tf.placeholder()
-X = tf.placeholder("float", [])
-Y = tf.placeholder("float", [])
+X = tf.placeholder(dtype = tf.float32)
+Y = tf.placeholder(dtype = tf.float32)
 
 c0 = tf.layers.conv2d(inputs = X,filers=64,kernel_size=3,activation = tf.nn.relu\
 kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv0")
@@ -26,24 +33,20 @@ kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = 
 
 fc0 = tf.contrib.layers.fully_connected(c2, 1, activation_fn=None)
 
-
-
-def training():
-    #filename =
-    return
-
 def load_data():
-    data = np.load( 'data/mnist_test_seq.npy' )
-    # ['clips', 'dims', 'input_raw_data']
+    global train_input_seq, train_output_seq, test_input_seq, test_output_seq
 
+    data = np.load( '../data/mnist_test_seq.npy' )
+    # ['clips', 'dims', 'input_raw_data']
     #(200K, 1, 64, 64) --> (10K, 20, 64, 64)-> number of sequences, frames/sequence, height, width
     data = np.reshape( data, [-1, 20, 64, 64, 1] )
     print("loading training data: data.shape", data.shape)
+    train_input_seq = data[0:8000, 0:10]
+    train_output_seq = data[0:8000, 10:]
+    test_input_seq = data[8000:, 0:10]
+    test_output_seq = data[8000:, 10:]
 
-    input_seq = data[:, 0:10]
-    output_seq = data[:, 10:]
-
-    return input_seq, output_seq
+    return
 
 
 #encoder
@@ -55,32 +58,34 @@ with tf.variable_scope("Encoder") as scope:
     for f in range(frames):
         if f > 0:
             scope.reuse_variables()
-        output, state = lstm(enc_in[f], state)
+        output, state = lstm(X[f], state)
+    output_decoder = output
+    state_decoder = state
+    output_future = output
+    state_future = state
 
 #decoder
 with tf.variable_scope("Decoder") as scope:
-# def decoder():
-    outputs = []
-    zeros = np.zeros()#shape
+    decoder_outputs = []
+
     for f in range(frames):
-        output, state = lstm(output, state)# state from encoder or new zero state?
-        outputs.append(output)
+        output_decoder,state_decoder = lstm(output_decoder, state_decoder)
+        decoder_outputs.append(output_decoder)
 
 with tf.variable_scope("FuturePredictor") as scope:
-# def decoder():
-    outputs = []
-    zeros = np.zeros()#shape
+    future_outputs = []
+
     for f in range(frames):
-        output, state = lstm(output, state)# state from encoder or new zero state?
-        outputs.append(output)
+        output_future, state_future = lstm(output_future, state_future)
+        future_outputs.append(output_future)
 
 #loss and optimization
 with tf.variable_scope("Loss") as scope:
 
-    loss = tf.reduced_sum(tf.square(outputs - Y))
+    loss = tf.reduced_sum(tf.square(decoder_outputs - Y))
     optim = tf.train.AdamOptimizer(lr).minimize(loss)
 
-
+load_data()
 
 #starting the session
 with tf.Session() as sess:
@@ -91,5 +96,3 @@ with tf.Session() as sess:
     for e in range(epochs):
 
         prediction = sess.run([pred],feed_dict = {})
-
-load_data()
