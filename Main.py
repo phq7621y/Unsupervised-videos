17b26e031baab63c67479943075fb1da9609594b
shreeyamody <modyshreeya@gmail.com>
2018-02-21 10:43:21 -0700

    added losses

--- Main.py
+++ Main.py
@@ -28,17 +28,17 @@ Y = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, 1])
 Y_future = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, 1])
 
 
-c0 = tf.reshape(X,[-1, 64, 64, 1])
-c0 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, \
-kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv0")
-
-c1 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, \
-kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv1")
-
-c2 = tf.layers.conv2d(inputs = c1,filters=64,kernel_size=3,activation = tf.nn.relu, \
-kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv2")
-
-fc0 = tf.contrib.layers.fully_connected(c2, 1, activation_fn=None)
+# c0 = tf.reshape(X,[-1, 64, 64, 1])
+# c0 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, \
+# kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv0")
+#
+# c1 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, \
+# kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv1")
+#
+# c2 = tf.layers.conv2d(inputs = c1,filters=64,kernel_size=3,activation = tf.nn.relu, \
+# kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv2")
+#
+# fc0 = tf.contrib.layers.fully_connected(c2, 1, activation_fn=None)
 
 
 def load_data():
@@ -65,7 +65,7 @@ with tf.variable_scope("Encoder") as scope:
     lstm = tf.contrib.rnn.LSTMCell(num_units = 4096)
     state = lstm.zero_state(batch_size,"float")
     datum = tf.split(X, frames, axis = 1)
-    
+
     for f in range(frames):
         if f > 0:
             scope.reuse_variables()
@@ -80,10 +80,11 @@ zero_input = tf.zeros_like(tf.reshape( datum[0], [batch_size, -1] ) , "float" ) 
 
 with tf.variable_scope("Decoder") as scope:
     decoder_outputs = []
-    
+
     for f in range(frames):
         output_decoder, state_decoder = lstm(zero_input, state_decoder)
         decoder_outputs.append(output_decoder)
+    decoder_outputs = tf.reshape(decoder_outputs,[batch_size,frames,-1])
 
 with tf.variable_scope("FuturePredictor") as scope:
     future_outputs = []
@@ -91,16 +92,20 @@ with tf.variable_scope("FuturePredictor") as scope:
     for f in range(frames):
         output_future, state_future = lstm(zero_input, state_future)
         future_outputs.append(output_future)
+    future_outputs = tf.reshape(future_outputs, [batch_size,frames,-1])
+
 
 #loss and optimization
 #with tf.variable_scope("Loss") as scope:
-    target = tf.reshape(tf.split(Y, frames, axis = 1), [frames, batch_size, -1])
-    target_future = tf.reshape(tf.split(Y_future, frames, axis = 1), [frames, batch_size, -1])
-    loss = tf.reduce_sum(tf.square(target - decoder_outputs))
-    loss_future = tf.reduce_sum(tf.square(target_future - future_outputs))
+    target = tf.reshape(tf.split(Y, frames, axis = 1), [batch_size,frames, -1])
+    target_future = tf.reshape(tf.split(Y_future, frames, axis = 1), [batch_size,frames, -1])
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(target - decoder_outputs), axis=[1,2]))  #l2 loss
+    loss_future = tf.reduce_mean(tf.reduce_sum(tf.square(target_future - future_outputs),axis = [1,2]))
     optim = tf.train.AdamOptimizer(lr).minimize(loss)
     optim_future = tf.train.AdamOptimizer(lr).minimize(loss_future)
-    
+
+
+
 load_data()
 
 #starting the session
@@ -110,9 +115,20 @@ with tf.Session() as sess:
     # summary_writer = tf.summary.FileWriter(_,graph=sess.graph)
 
     for e in range(epochs):
-        for i in range(int(len(train_input_seq)/batch_size)):
+        num_batches = int(len(train_input_seq)/batch_size)
+        avg_losses = []
+        total_loss = 0
+        avg_losses_future = []
+        total_loss_future = 0
+        for i in range(num_batches):
             x_train = train_input_seq[i*batch_size:(i+1)*batch_size]
             y_train = train_output_seq[i*batch_size:(i+1)*batch_size]
             y_train_future = train_future_seq[i*batch_size:(i+1)*batch_size]
-            train_optim = sess.run([optim, optim_future],feed_dict = {X: x_train, Y: y_train, Y_future: y_train_future})
-            
+            batch_loss,batch_loss_future,train_optim,train_optim_future = sess.run([loss,loss_future,optim, optim_future],\
+                feed_dict = {X: x_train, Y: y_train, Y_future: y_train_future})
+            total_loss += batch_loss
+            total_loss_future += batch_loss_future
+        avg_losses.append(total_loss/num_batches)
+        avg_losses_future.append(total_loss_future/num_batches)
+
+        
