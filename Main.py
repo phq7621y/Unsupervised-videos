e9234996df82b92bd6a015aba4432d5e0da81783
shreeyamody <modyshreeya@gmail.com>
2018-03-01 14:11:05 -0700

    adding plotting code

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 #import random
 #import math
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from skimage import io, transform
 
 tf.reset_default_graph()
@@ -15,7 +15,7 @@ train_future_seq = [None]
 test_input_seq = [None]
 test_output_seq = [None]
 test_future_seq = [None]
-dim = 1
+channel = 1
 
 def load_data():
     global train_input_seq, train_output_seq, train_future_seq, test_input_seq, test_output_seq, test_future_seq
@@ -23,14 +23,15 @@ def load_data():
     data = np.load( '/datasets/mnist_test_seq.npy' )
     # ['clips', 'dims', 'input_raw_data']
     #(200K, 1, 64, 64) --> (10K, 20, 64, 64)-> number of sequences, frames/sequence, height, width
-    data = np.reshape( data, [-1, 20, 64, 64, dim] )
+    data = np.reshape( data, [-1, 20, 64, 64, channel] )
     print("loading training data: data.shape", data.shape)
     train_input_seq = data[0:10000, 0:10]
     train_output_seq = train_input_seq[0:10000, ::-1]
     train_future_seq = data[0:10000, 10:]
-#    test_input_seq = data[8000:, 0:10]
-#    test_output_seq = test_input_seq[8000:, ::-1]
-#    test_future_seq = data[8000:,10:]
+
+    test_input_seq = data[10000:, 0:10]
+    test_output_seq = test_input_seq[10000:, ::-1]
+    test_future_seq = data[10000:,10:]
 
     return
 
@@ -38,18 +39,18 @@ load_data()
 
 #parameters
 batch_size = 64
-epochs = 2
+epochs = 10
 frames = 10
 lr = 0.01
 
 #placeholders
 #enc_in = tf.placeholder()
-X = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, dim])
-Y = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, dim])
-Y_future = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, dim])
+X = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, channel])
+Y = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, channel])
+Y_future = tf.placeholder(tf.float32, [batch_size, frames, 64, 64, channel])
 
-
-c0 = tf.reshape(X,[-1, 64, 64, dim])
+#
+c0 = tf.reshape(X,[-1, 64, 64, channel])
 c0 = tf.layers.conv2d(inputs = c0,filters=24,kernel_size=5,activation = tf.nn.relu, strides=(2,2), \
 kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "d_conv0")
 
@@ -64,7 +65,7 @@ def fully_connected(input, reuse=False):
     with tf.variable_scope("fc", reuse=reuse):
         out = tf.contrib.layers.fully_connected(input, 4096, activation_fn=None)
         out = tf.reshape(out, [batch_size, frames, -1])
-    
+
     return out
 
 inp = tf.reshape(c2, [batch_size, frames, -1])
@@ -87,7 +88,7 @@ with tf.variable_scope("Encoder") as scope:
 
 #decoder
 zero_input = tf.zeros_like(tf.reshape( datum[0], [batch_size, -1] ) , "float" ) # generate a zero array using the shape of tmp
-    
+
 with tf.variable_scope("Decoder") as scope:
     decoder_outputs = []
 
@@ -96,7 +97,7 @@ with tf.variable_scope("Decoder") as scope:
             scope.reuse_variables()
         output_decoder, state_decoder = lstm(zero_input, state_decoder)
         decoder_outputs.append(output_decoder)
-    decoder_outputs = tf.reshape(decoder_outputs,[batch_size*frames,-1]) 
+    decoder_outputs = tf.reshape(decoder_outputs,[batch_size*frames,-1])
 decoder_outputs = fully_connected(decoder_outputs)
 
 with tf.variable_scope("FuturePredictor") as scope:
@@ -131,33 +132,31 @@ with tf.Session() as sess:
     img_pre = [None]
     img = [None]
     img_future = [None]
-    
+
     for e in range(epochs):
         print(e)
         iteration.append(e)
         num_batches = int(len(train_input_seq)/batch_size)
         total_loss = 0
         total_loss_future = 0
-        
+
         for i in range(num_batches):
             x_train = train_input_seq[i*batch_size:(i+1)*batch_size]
-            
+
             y_train = train_output_seq[i*batch_size:(i+1)*batch_size]
             y_train_future = train_future_seq[i*batch_size:(i+1)*batch_size]
             batch_loss,batch_loss_future,train_optim,train_optim_future = sess.run([loss,loss_future,optim, optim_future],\
                 feed_dict = {X: x_train, Y: y_train, Y_future: y_train_future})
-                
+
             total_loss += batch_loss
             total_loss_future += batch_loss_future
-        
+
         avg_losses.append(total_loss/num_batches)
         avg_losses_future.append(total_loss_future/num_batches)
 
-
-'''
-    img_pre = train_input_seq[0:batch_size]
+    img_pre = test_input_seq[0:batch_size]
     img, img_future = sess.run([decoder_outputs, future_outputs], feed_dict = {X: img_pre})
-    
+
 print(iteration)
 print(avg_losses)
 
@@ -176,9 +175,9 @@ plt.xlabel("epoch")
 plt.show()
 plt.savefig('decoder_future.png')
 
-img_pre = np.reshape( img_pre, [batch_size, frames, 64, 64, dim] )
-img = np.reshape( img, [batch_size, frames, 64, 64, dim] )
-img_future = np.reshape( img_future, [batch_size, frames, 64, 64, dim] )
+img_pre = np.reshape( img_pre, [batch_size, frames, 64, 64, channel] )
+img = np.reshape( img, [batch_size, frames, 64, 64, channel] )
+img_future = np.reshape( img_future, [batch_size, frames, 64, 64, channel] )
 
 source = []
 reverse = []
@@ -188,13 +187,13 @@ for t in range(frames):
     source.append(img_pre[0,t])
     reverse.append(img[0,t])
     future.append(img_future[0,t])
-    
+
 source = np.concatenate(source)
 reverse = np.concatenate(reverse)
 future = np.concatenate(future)
-    
+
 plt.imsave("Source.png", source)
-plt.imsave("Reverse.png", reverse)    
+plt.imsave("Reverse.png", reverse)
 plt.imsave("Future.png", future)
 
 ### goes in a file outside supercomputer
@@ -207,14 +206,13 @@ plt.imsave("Future.png", future)
 # al.close()
 # alf.close()
 
-'''
-
-with open('avg_losses', 'wb') as al:
-    pickle.dump(avg_losses, al)
-
-
-with open('avg_losses_future', 'wb') as alf:
-    pickle.dump(avg_losses_future, alf)
-
-al.close()
-alf.close()
+#
+# with open('avg_losses', 'wb') as al:
+#     pickle.dump(avg_losses, al)
+#
+#
+# with open('avg_losses_future', 'wb') as alf:
+#     pickle.dump(avg_losses_future, alf)
+#
+# al.close()
+# alf.close()
